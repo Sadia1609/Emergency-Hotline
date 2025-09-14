f8e80a6586c0a53ca69057e532ee9ceb884be0f9
Sadia1609 <sadia.rahman160920@gmail.com>
2025-09-15 00:38:32 +0600

    added call btn functionality

--- script.js
+++ script.js
@@ -7,7 +7,38 @@ function setInnerText(id, value) {
 
 }
 
+// heart icons
 function heartIcon(){
     heart++;
     setInnerText("heartCount", heart);
 }
+
+
+//call btn
+function callBtn(number) {
+    if(coin < 20){
+        alert("Nogt enough coin");
+        return;
+    }
+
+    coin -= 20;
+    document.getElementById("coin").innerText = coin;
+    const cards = document.querySelectorAll("main section > div > div");
+    let serviceName = "";
+
+    for(let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+
+        const pElement = card.querySelector("p");
+        if(pElement && pElement.innerText === number){
+            const h3 = card.querySelector("h3");
+            if(h3) serviceName = h3.innerText;
+            break;
+        }
+    }
+    alert("Calling: " + number + serviceName);
+}
+
+
+
+
